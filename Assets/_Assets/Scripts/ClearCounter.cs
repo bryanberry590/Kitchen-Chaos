b3db919a1dd4a4c6eec0038c9b58d9613e5ee263
bryanberry590 <144753561+bryanberry590@users.noreply.github.com>
2025-03-18 18:36:20 -0700

    Initial commit

---
+++ Assets/_Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,56 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ClearCounter : MonoBehaviour, IKitchenObjectParent
+{
+    
+    
+    [SerializeField] private KitchenObjectSO kitchenObjectSo;
+    [SerializeField]private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+    
+    
+    public void Interact(Player player)
+    {
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+        }
+        else
+        {
+            //give object to player
+            kitchenObject.SetKitchenObjectParent(player);
+
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+    
+}
+
+
---
+++ Assets/_Assets/Scripts/Player.cs
@@ -0,0 +1,173 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Player : MonoBehaviour, IKitchenObjectParent
+{
+
+    public static Player Instance { get; private set; }
+    public event EventHandler<OnSelectedCounterChangeEventArgs> OnSelectedCounterChanged;
+
+    public class OnSelectedCounterChangeEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+        
+    }
+    
+    [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
+    [SerializeField] private PlayerInput playerInput;
+    private Vector3 lastInteractDirection;
+    [SerializeField] private LayerMask countersLayerMask;
+    private ClearCounter selectedCounter;
+    private KitchenObject kitchenObject;
+    [FormerlySerializedAs("counterTopPoint")] [SerializeField]private Transform KitchenObjectHoldPoint;
+
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than one instance of Player");
+        }
+        Instance = this;
+        
+    }
+    private void Start()
+    {
+        playerInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+
+
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if (selectedCounter != null)
+        {
+            selectedCounter.Interact(this);
+        }
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = playerInput.GetMovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDirection = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit hitInfo, interactDistance, countersLayerMask))
+        {
+            if (hitInfo.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // interacting with clear counter
+                if (clearCounter != selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
+                
+            }
+            else
+            {
+                SetSelectedCounter(null);
+            }
+        }
+        else
+        {
+            SetSelectedCounter(null);
+        }
+        
+    }
+    
+    private void HandleMovement()
+    {
+        Vector2 inputVector = playerInput.GetMovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance );
+
+        if (!canMove)
+        {
+            //attempt only x movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance );
+
+            if (canMove)
+            {
+                //can move only on the x axis
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //attempt only z movement
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance );
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        
+        isWalking = moveDir != Vector3.zero;
+        
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangeEventArgs { selectedCounter = selectedCounter });
+
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return KitchenObjectHoldPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
